bb4ad124ca1f8a01a206abf865154132b680fc08
SnackCode <dylan.frankland@gmail.com>
2014-05-08 10:07:16 -0700

    Oops
    Get it? OOP? Nevermind… Made some functions protected so that they
    don't get redeclared by my other modules.

--- app/code/community/SnackCode/OpenERPConnectorMagentoInvoiceAutoCapture/Model/Observer.php
+++ app/code/community/SnackCode/OpenERPConnectorMagentoInvoiceAutoCapture/Model/Observer.php
@@ -2,6 +2,43 @@
 /*Written by Dylan Frankland - dylan.frankland+SnackCode@gmail.com*/
 class SnackCode_OpenERPConnectorMagentoInvoiceAutoCapture_Model_Observer
 {
+    /*email function to alert of failed captures*/
+    protected function sendEmail($subject,$body)
+    {
+        $mail = Mage::getModel('core/email');
+        $mail->setToName(Mage::getStoreConfig('trans_email/ident_support/name'));
+        $mail->setToEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
+        $mail->setFromEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
+        $mail->setFromName(Mage::app()->getStore()->getName());
+        $mail->setBody($body);
+        $mail->setSubject($subject);
+        $mail->setType('html');// You can use 'html' or 'text'
+        try
+        {
+            $mail->send();
+        }
+        catch (Exception $e)
+        {
+            Mage::logException('OpenERP Connector - Magento: Invoice AutoCapture // Exception message: '.$e->getMessage());
+        }
+    }
+    
+    /*save everything after you have altered it in any way*/
+    protected function saveEverything($order,$payment,$invoice)
+    {
+        Mage::getModel('core/resource_transaction')->addObject($invoice)->addObject($payment)->addObject($order)->save();
+    }
+    
+    /*log every success or issue using email, mage log at /var/log/system.log, and also the order comments*/
+    protected function logEverything($subject,$body,$oid,$order)
+    {
+        $oid = 'Order #'.$oid.' ';
+        $this->sendEmail($oid.$subject,$body);
+        Mage::log($oid.$subject,Zend_Log::DEBUG,'OpenERPConnectorMagentoInvoiceAutoCapture.log',true);
+        $order->addStatusHistoryComment($subject.$body, true);
+        $this->saveEverything($order,$payment,$invoice);
+    }
+    
     public function invoiceAutoCapture($observer)
     {
         /*get all order/invoice/payment details from the observer*/
@@ -10,42 +47,6 @@ class SnackCode_OpenERPConnectorMagentoInvoiceAutoCapture_Model_Observer
         $order = $invoice->getOrder();
         $payment = $order->getPayment();
         $orderId = $invoice->getOrder()->getIncrementId();
-        
-        /*email function to alert of failed captures*/
-        function sendEmail($subject,$body){
-            $mail = Mage::getModel('core/email');
-            $mail->setToName(Mage::getStoreConfig('trans_email/ident_support/name'));
-            $mail->setToEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
-            $mail->setFromEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
-            $mail->setFromName(Mage::app()->getStore()->getName());
-            $mail->setBody($body);
-            $mail->setSubject($subject);
-            $mail->setType('html');// You can use 'html' or 'text'
-            try
-            {
-                $mail->send();
-            }
-            catch (Exception $e)
-            {
-                Mage::logException('OpenERP Connector - Magento: Invoice AutoCapture // Exception message: '.$e->getMessage());
-            }
-        }
-        
-        /*log every success or issue using email, mage log at /var/log/system.log, and also the order comments*/
-        function logEverything($subject,$body,$oid,$order)
-        {
-            $oid = 'Order #'.$oid.' ';
-            sendEmail($oid.$subject,$body);
-            Mage::log($oid.$subject,Zend_Log::DEBUG,'OpenERPConnectorMagentoInvoiceAutoCapture.log',true);
-            $order->addStatusHistoryComment($subject.$body, true);
-            saveEverything($order,$payment,$invoice);
-        }
-        
-        /*save everything after you have altered it in any way*/
-        function saveEverything($order,$payment,$invoice)
-        {
-            Mage::getModel('core/resource_transaction')->addObject($invoice)->addObject($payment)->addObject($order)->save();
-        }
         
         /*Try to capture paypal payment from order invoice and email problems*/
         if($payment->canCapture() && !$order->canInvoice())
@@ -60,7 +61,7 @@ class SnackCode_OpenERPConnectorMagentoInvoiceAutoCapture_Model_Observer
                 $success = false;
                 $subject = 'OpenERP Connector - Magento: Invoice AutoCapture // Exception Message';
                 $body = $e->getMessage();
-                logEverything($subject,$body,$orderId,$order);
+                $this->logEverything($subject,$body,$orderId,$order);
             }
             if($success)
             {
@@ -68,14 +69,14 @@ class SnackCode_OpenERPConnectorMagentoInvoiceAutoCapture_Model_Observer
                 $order->setIsInProcess(true);
                 $order->addStatusHistoryComment('OpenERP Connector - Magento: Invoice AutoCapture // Success!', true);
                 $invoice->sendEmail();
-                saveEverything($order,$payment,$invoice);
+                $this->saveEverything($order,$payment,$invoice);
             }
         }
         else
         {
             $subject = 'OpenERP Connector - Magento: Invoice AutoCapture // Cannot Capture Paypal Payment!';
             $body = 'Authorization failed, or the invoice is not yet created. $invoice->canCapture()=false.';
-            logEverything($subject,$body,$orderId,$order);
+            $this->logEverything($subject,$body,$orderId,$order);
         }
         return $this;
     }
